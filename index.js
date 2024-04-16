5db2f6cb4b8eb014b87c33a57e5e74faaa7d139e
woody <gelius24@hotmail.com>
2024-04-16 07:20:32 -0400

    play/stop bg video

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const spotify = document.querySelector('#spotify')
 const blog = document.querySelector('#mernblog')
 const amazon = document.querySelector('#amazon')
 const netflix = document.querySelector('#netflix')
+const video = document.querySelector('video')
+const vidController = document.querySelector('.video-controller')
 
 spotify.addEventListener('click', () => {
   window.open('https://spotify-clone-wg.onrender.com/', '_blank');
@@ -18,3 +20,14 @@ amazon.addEventListener('click', () => {
 netflix.addEventListener('click', () => {
   window.open('https://netflix-full-build-29831.web.app/', '_blank');
 })
+
+vidController.addEventListener('click', () => {
+  if (video.paused) {
+    video.play()
+    vidController.innerHTML = '<i class="fa-solid fa-pause"></i>'
+  } else {
+    video.pause()
+    vidController.innerHTML = '<i class="fa-solid fa-play"></i>'
+  }
+})
+
